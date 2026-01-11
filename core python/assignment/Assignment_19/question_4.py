24389b0a20b4673a8da5b6a2d42ab7c9fe9611f8
Shital chougule <chouguleshital2203@gmail.com>
2026-01-11 16:10:09 +0530

    Delete core python/assignment/Assignment_19 directory

--- core python/assignment/Assignment_19/question_4.py
+++
@@ -1,7 +0,0 @@
-#4 : Remove all the vowels from a string
-input=str (input("Enter a string: "))
-count=0
-for char in input:
-    if char in 'aeiouAEIOU':
-        input = input.replace(char, "")
-print("String after removing vowels:", input)
